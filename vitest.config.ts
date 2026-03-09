8ca9cea95af8022e74338e431a941125153e52cf
Carl Dawson <106377191+RolfMoleman@users.noreply.github.com>
2026-03-09 09:29:34 +0000

    chore(config): 🔧 add vitest configuration file

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["tests/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html", "lcov"],
+      exclude: [
+        "node_modules/**",
+        "dist/**",
+        "**/*.test.ts",
+        "vitest.config.ts",
+      ],
+    },
+  },
+});
